239792d341aabf33eabb22c1a6098177c73a7154
Mcpie <mcpie87@gmail.com>
2025-02-22 18:12:52 +0100

    cd

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  output: 'export',
+  trailingSlash: true,
+  basePath: '',
 };
 
 export default nextConfig;
