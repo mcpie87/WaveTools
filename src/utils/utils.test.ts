06880cd4d6d45825074bc794556765f32a961987
Mcpie <mcpie87@gmail.com>
2025-03-03 00:34:02 +0100

    added test for getKeyFromEnumValue

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,4 +1,4 @@
-import { convertToUrl } from "./utils";
+import { convertToUrl, getKeyFromEnumValue } from "./utils";
 
 const ASSET_URL = "https://raw.githubusercontent.com/alt3ri/WW_Asset/refs/heads/Global/";
 
@@ -18,4 +18,28 @@ describe('utils test', () => {
     expect(convertToUrl(urls[3])).toBe(ASSET_URL + "UIResources/Common/Image/IconRoleHeadCircle256/T_IconRoleHeadCircle256_8_UI.png");
     expect(convertToUrl(urls[4])).toBe(ASSET_URL + "UIResources/Common/Image/IconRoleHead256/T_IconRoleHead256_7.png");
   });
+
+  test('getKeyFromEnumValue', () => {
+    enum ActiveSkillNames {
+      normalAttack = "Normal Attack",
+      resonanceSkill = "Resonance Skill",
+      resonanceLiberation = "Resonance Liberation",
+      forte = "Forte",
+      intro = "Intro",
+    }
+    enum PassiveSkillNames {
+      inherent = "Inherent",
+      side1 = "Side1", // far left
+      side2 = "Side2", // 2nd left
+      side3 = "Side3", // 2nd right
+      side4 = "Side4", // far right
+    }
+    const enums = [ActiveSkillNames, PassiveSkillNames];
+    for (const enumObj of enums) {
+      for (const [key, value] of Object.entries(enumObj)) {
+        // @ts-expect-error value is cancer to type in this test
+        expect(getKeyFromEnumValue(enumObj, value)).toBe(key);
+      }
+    }
+  })
 });
