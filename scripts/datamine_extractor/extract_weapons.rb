b4b03340acebbb297b69bce003a9b4804a81184d
Mcpie <mcpie87@gmail.com>
2025-03-05 00:14:09 +0100

    extracted ascension items from weapons

--- scripts/datamine_extractor/extract_weapons.rb
+++ scripts/datamine_extractor/extract_weapons.rb
@@ -1,19 +1,49 @@
 require_relative 'utils'
 
 weapon_types = load_file("#{DATAMINE_PATH}/#{BINDATA}/mapping/mapping.json", true)
-    .select{|row| row["FieldName"] == "WeaponType"}
-    .map{|e|
-    {
-        id: e["Value"],
-        name: get_textmap_name(e["Comment"]),
-        icon: convert_to_png(e["Icon"])
-    }}
-    .map{|e| [e[:id], e]}
-    .to_h
+  .select{|row| row["FieldName"] == "WeaponType"}
+  .map{|e|
+  {
+    id: e["Value"],
+    name: get_textmap_name(e["Comment"]),
+    icon: convert_to_png(e["Icon"])
+  }}
+  .map{|e| [e[:id], e]}
+  .to_h
 
 
-# resonator
+# weapons
 @weapon_info = load_file("#{DATAMINE_PATH}/#{BINDATA}/weapon/weaponconf.json", true)
+# ascension
+@weapon_breach_info = load_file("#{DATAMINE_PATH}/#{BINDATA}/weapon/weaponbreach.json", true);
+# general item info
+@iteminfo = load_file("#{DATAMINE_PATH}/#{BINDATA}/item/iteminfo.json")
+
+def get_weapon_breach_info(weapon_id)
+  @weapon_breach_info
+    .select{|e| e["BreachId"] == weapon_id}
+    .map{|row|
+      {
+        rank: row["Level"],
+        max: row["LevelLimit"],
+        items: [
+          {
+            id: 2,
+            name: get_textmap_name(@iteminfo[2]["Name"]),
+            value: row["GoldConsume"]
+          },
+          row["Consume"].map{|mat|
+            {
+              id: mat["Key"],
+              name: get_textmap_name(@iteminfo[mat["Key"]]["Name"]),
+              value: mat["Value"],
+            }
+          }
+        ].flatten
+      }
+    }
+    .sort_by { |row| row[:rank] }
+end
 
 data = []
 @weapon_info.each do |weapon_value|
@@ -27,6 +57,7 @@ data = []
             middle: convert_to_png(weapon_value["IconMiddle"]),
             small: convert_to_png(weapon_value["IconSmall"]),
         },
+        materials: get_weapon_breach_info(weapon_value["ItemId"])
     }
     data << new_row
     puts("#{new_row[:id]}  #{new_row[:rarity]} #{new_row[:name]}")
