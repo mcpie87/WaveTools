06b271ee32f2f9f73134185c0a143cc1d12fae8f
Mcpie <mcpie87@gmail.com>
2025-03-04 17:25:56 +0100

    repository name change

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 const isProduction = process.env.NODE_ENV === 'production';
-const repositoryName = 'wuwa-tracker';
+const repositoryName = 'WaveTools';
 
 const nextConfig: NextConfig = {
   /* config options here */
