b1086267fcb18e34629ad9f2a0fff7eab6aea7cf
Mcpie <mcpie87@gmail.com>
2025-03-04 18:36:09 +0100

    test

--- src/providers/DataProvider.tsx
+++ src/providers/DataProvider.tsx
@@ -15,10 +15,16 @@ export const DataProvider = ({ children }: DataProviderProps) => {
   useEffect(() => {
     async function fetchData() {
       try {
-        const itemsResponse = await fetch(`/data/items.json`);
+        const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+        console.log("fetchData", "basePath", basePath, "env", process.env.NEXT_PUBLIC_BASE_PATH)
+        const itemsResponse = await fetch(`${basePath}/data/items.json`);
+        const itemsResponse2 = await fetch(`/data/items.json`);
+        const itemsResponse3 = await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH}/data/items.json`);
         const itemsDb: IAPIItem[] = await itemsResponse.json();
+        console.log(itemsResponse2?.json());
+        console.log(itemsResponse3?.json());
 
-        const resonatorResponse = await fetch(`/data/resonator.json`);
+        const resonatorResponse = await fetch(`${basePath}/data/resonator.json`);
         const resonatorDb: IAPIResonator[] = await resonatorResponse.json();
 
         setData({
