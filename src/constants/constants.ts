3eb230368f1937ccf35761a67006f8268010688e
Mcpie <mcpie87@gmail.com>
2025-07-08 20:16:47 +0200

    restored old asset url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const ASSET_URL = "https://git.encore.moe/alt3ri/WW_Asset/-/raw/Global/";
+export const ASSET_URL = "https://raw.githubusercontent.com/alt3ri/WW_Asset/refs/heads/Global/";
 
 export const WAVEPLATE_ICON_URL = "/Game/Aki/UI/UIResources/Common/Image/IconA80/T_IconA80_yml_UI.png";
 export const SHELL_CREDIT_ICON_URL = "/Game/Aki/UI/UIResources/Common/Image/IconA/T_IconA_hsb_UI.png";
