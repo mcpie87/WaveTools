4b9904d4e02c9d5ac0f4573bbb08b7bbb45fb095
Mcpie <mcpie87@gmail.com>
2026-02-15 03:57:09 +0100

    added missing map type

--- src/types/mapTypes.ts
+++ src/types/mapTypes.ts
@@ -1,5 +1,9 @@
+import { __ALL_MAPS__, DungeonName, MapName } from "@/app/map/mapUtils";
+
 export interface DbMapData {
   visibleCategories: Record<string, boolean>,
   visitedMarkers: Record<number, boolean>,
   displayedCategoryGroups: Record<string, boolean>,
 }
+
+export type SelectedMap = MapName | DungeonName | typeof __ALL_MAPS__;
