e012656094fc4f05c17f1840cff2e1ba05455214
Mcpie <mcpie87@gmail.com>
2026-05-05 18:39:48 +0200

    feat: added sabercat prowler challenge in dimmr plains

--- src/app/map/TranslationMaps/EnemyChallenges.ts
+++ src/app/map/TranslationMaps/EnemyChallenges.ts
@@ -5,27 +5,35 @@ export enum EnemyChallenges {
   TWIN_NOVA_COLLAPSAR_BLADE = "Twin Nova: Collapsar Blade",
   ROYAN_WOMAN = "Royan Woman",
   ROYAN_MAN = "Royan Man",
+  SABERCAT_PROWLER = "Sabercat Prowler",
 }
 
 const EnemyChallengesTranslationMapGroups: Record<string, { keys: string[]; key: string }> = {
   // 3.3
   [EnemyChallenges.TWIN_NOVA_COLLAPSAR_BLADE]: {
-    key: "ECHO_TWIN_NOVA_COLLAPSAR_BLADE",
+    key: "ENEMY_CHALLENGE_TWIN_NOVA_COLLAPSAR_BLADE",
     keys: [
       "branch3.3_607_Monster5", // might be the other type
       "branch3.3_607_Monster4", // might be the other type
     ]
   },
   [EnemyChallenges.ROYAN_WOMAN]: {
-    key: "ECHO_ROYAN_WOMAN",
+    key: "ENEMY_CHALLENGE_ROYAN_WOMAN",
     keys: [
       "branch3.3_607_Monster_Branch3.0_013",
     ]
   },
   [EnemyChallenges.ROYAN_MAN]: {
-    key: "ECHO_ROYAN_MAN",
+    key: "ENEMY_CHALLENGE_ROYAN_MAN",
     keys: [
       "branch3.3_607_Monster_Branch3.0_012",
+    ]
+  },
+  [EnemyChallenges.SABERCAT_PROWLER]: {
+    key: "ENEMY_CHALLENGE_SABERCAT_PROWLER",
+    keys: [
+      "branch3.3_607_Monster_monster3_3",
+      "branch3.3_607_Monster_monster3_5",
     ]
   },
 }
