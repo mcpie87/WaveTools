e16aa78b220bfd05dedbf3d80bd14ee4416fd5cb
Mcpie <mcpie87@gmail.com>
2026-05-11 03:17:11 +0200

    datamine: modified levelplaynode extractor

--- scripts/datamine_extractor/extract_leveldata.rb
+++ scripts/datamine_extractor/extract_leveldata.rb
@@ -2,6 +2,17 @@ require_relative 'utils'
 require 'set'
 
 @levelplaydata_config = load_file("#{DATAMINE_PATH}/#{BINDATA}/LevelPlayData/levelplaydata.json", true)
+@levelplaynodedata_config = load_file("#{DATAMINE_PATH}/#{BINDATA}/LevelPlayNodeData/levelplaynodedata.json", true)
+
+levelplaynode_preprocess_data = {}
+@levelplaynodedata_config.each do |row|
+  key, idx = row["Key"].split("_").map(&:to_i)
+  levelplaynode_preprocess_data[key] ||= []
+  translation = get_textmap_name(row["Data"]["TidTip"])
+  next if translation == "" || translation == "NO CONTENT"
+  levelplaynode_preprocess_data[key] << [idx, translation]
+end
+
 
 data = []
 @levelplaydata_config.each do |row|
@@ -17,6 +28,8 @@ data = []
     Reference: rowData["Reference"],
     Children: rowData["Children"],
     Type: rowData["Type"],
+    Translations: levelplaynode_preprocess_data[rowData["Id"]].sort_by{|e| e[0]},
+    Condition: rowData.dig("LevelPlayOpenCondition", "Conditions")
   }
 end
 
