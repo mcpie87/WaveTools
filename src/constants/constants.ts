f543dd14f7da8e9dc8b2de264315a212a65d73e2
Mcpie <mcpie87@gmail.com>
2026-07-08 14:11:24 +0200

    feat: 3.5 levelentityconfig upload

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,5 @@
 export const ASSET_URL = "https://raw.githubusercontent.com/alt3ri/WW_Asset_Webp/refs/heads/main/";
-export const GAME_VERSION = "3.4";
+export const GAME_VERSION = "3.5";
 export const NO_DATA_STRING = "NO CONTENT"
 
 
@@ -15,4 +15,5 @@ export const LEVEL_ENTITY_CONFIG_URL = {
   "3.2": "https://wwfmp0c1vm.ufs.sh/f/GKKXYOQgq7aYg2T5sklxJ4nuj2VU30S6CMmriOek7xaNolKR",
   "3.3": "https://wwfmp0c1vm.ufs.sh/f/GKKXYOQgq7aY5fi4u2qjAdJkHTRCp4gGhObslNFKLmSfDMu9",
   "3.4": "https://wwfmp0c1vm.ufs.sh/f/GKKXYOQgq7aYltkvqNvoarfhQgc5ZKqovzUx7Ib2J6CmBEdy",
+  "3.5": "https://wwfmp0c1vm.ufs.sh/f/GKKXYOQgq7aYa0zIDLV2ueLCWN7KzdQgiRDxGBj5wfln8USM",
 };
